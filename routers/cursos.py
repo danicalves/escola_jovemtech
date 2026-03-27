6a045130e06b15ccd18ec48cf1ff0ba5773a4122
danicalves <daniel.costa.alves@gmail.com>
2026-03-27 15:27:51 -0300

    fix: logger name error

--- routers/cursos.py
+++ routers/cursos.py
@@ -10,7 +10,7 @@ import logging
 
 # Configuração de logging
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(_name_)
+logger = logging.getLogger(__name__)
 
 cursos_router = APIRouter()
 
